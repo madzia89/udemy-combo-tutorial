6b944f3f6fcc172c51c1cfccd32f9a224275591e
Magda Wójcicka <magda.wojcicka@britenet.com.pl>
2019-02-12 10:39:26 +0100

    login

--- routes/api/users.js
+++ routes/api/users.js
@@ -52,4 +52,35 @@ router.post('/register', (req, res) => {
         })
 });
 
+
+//@route GET api/users/login
+//@desc Login User / return JWT token
+// @access Public
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //Find user by email
+    User.findOne({email})
+        .then(user => {
+            //check for user
+            if (!user) {
+                return res.status(404).json({email: 'User not found'})
+            }
+
+            //Check password
+            //compare if given password matches password provided by th user
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({msg: 'Success'});
+                    } else {
+                        return res.status(400).json({password: 'Password incorrect'});
+                    }
+                })
+        })
+});
+
+
 module.exports = router;
