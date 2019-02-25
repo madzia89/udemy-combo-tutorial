e4d9fbf13c949cfa5613880018fcd675fe1d49f1
Magda Wójcicka <magda.wojcicka@britenet.com.pl>
2019-02-25 16:03:54 +0100

    post fixes and validation

--- models/Post.js
+++ models/Post.js
@@ -2,17 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create Schema
-const PostShcema = newShcema({
+const PostSchema = new Schema({
     user: {
-        type: Schema.Types.ObjectId, //get user by id
-        ref: 'user' //reference to user collection
+        type: Schema.Types.ObjectId,
+        ref: 'users'
     },
     text: {
         type: String,
         required: true
     },
     name: {
-        // Name is passed here so even if the user deletes his profile, the post will stay and be signed by his name
         type: String
     },
     avatar: {
@@ -20,26 +19,23 @@ const PostShcema = newShcema({
     },
     likes: [
         {
-            // Users are passed so there can be a logic that prevents from multiliking or disliking
             user: {
-                type: Schema.Types.ObjectId, //get user by id
-                ref: 'user' //reference to user collection
+                type: Schema.Types.ObjectId,
+                ref: 'users'
             }
         }
     ],
     comments: [
         {
-            // Users are passed so there can be a logic that prevents from multiliking or disliking
             user: {
-                type: Schema.Types.ObjectId, //get user by id
-                ref: 'user' //reference to user collection
+                type: Schema.Types.ObjectId,
+                ref: 'users'
             },
             text: {
-                String,
+                type: String,
                 required: true
             },
             name: {
-                // Name is passed here so even if the user deletes his profile, the post will stay and be signed by his name
                 type: String
             },
             avatar: {
@@ -47,10 +43,14 @@ const PostShcema = newShcema({
             },
             date: {
                 type: Date,
-                default: Date.now,
+                default: Date.now
             }
         }
-    ]
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = Post = mongoose.model('post', PostSchema)
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,10 +2,40 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Post model
+const Post = require('../../models/Post');
+
+// Validation
+const validatePostInput = require('../../validation/post');
 
 //@route GET api/posts/test
 //@desc Tests posts route
 //@access Public
 router.get('/test', (req, res) => res.json({msg: "Posts works"}));
+
+//@route GET api/posts
+//@desc Create Post
+//@access Private
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+    const {errors, isValid} = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        // if any errors, sen 400 with errors object
+        return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post)).catch(err => res.status(400).json('Wystąpił błąd'));
+});
 
 module.exports = router;
