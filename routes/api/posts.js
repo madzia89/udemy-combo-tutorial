b5ee524cec79652835b8ca159c0512f574aa19b0
Magda Wójcicka <magda.wojcicka@britenet.com.pl>
2019-02-26 09:08:43 +0100

    delete post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,6 +8,9 @@ const passport = require('passport');
 // Post model
 const Post = require('../../models/Post');
 
+
+const Profile = require('../../models/Profile');
+
 // Validation
 const validatePostInput = require('../../validation/post');
 
@@ -57,6 +60,27 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     });
 
     newPost.save().then(post => res.json(post)).catch(err => res.status(400).json('Wystąpił błąd'));
+});
+
+
+//@route DELETE api/posts/:id
+//@desc Delete post by id
+//Private
+router.delete('/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+    //tylko właściciel postu może usuwać posty
+    Profile.findOne({user: req.user.id})
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    //sprawdź właściciela postu
+                    if (post.user.toString() !== req.user.id) {
+                        return res.status(401).json({notauthorized: 'User not authorized to remove post'})
+                    }
+                    //DELETE
+                    Post.deleteOne().then(() => res.json({success: true}))
+                })
+                .catch(err => res.status(404).json({postnotfound: 'Post not found'}))
+        })
 });
 
 module.exports = router;
