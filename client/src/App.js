82581ac9a2ec6f78316d118d900eee8b8004615c
Magda Wójcicka <magda.wojcicka@britenet.com.pl>
2019-03-01 10:02:52 +0100

    add routing to login, register and landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react';
+//Browser Router pozwala np. na zastosowanie przyciskyu wstecz
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
 import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer"
 import Landing from "./components/layout/Landing"
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-        <div>
-            <NavBar/>
-            <Landing/>
-            <Footer/>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <NavBar/>
+                    <Route exact path={"/"} component={Landing}/>
+                    <div className={"container"}>
+                        <Route exact path={"/register"} component={Register}/>
+                        <Route exact path={"/login"} component={Login}/>
+                    </div>
+                    <Footer/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
